78c9b888570d409f3641fa8effcedebd38691170
seogyeong1 <82697951+seogyeong1@users.noreply.github.com>
2024-04-05 17:18:01 +0900

    [FEAT] ShoppingVIewController Input Output Pattern 분리 TODO: Cell 에 관련한 기능

--- SeSACRxThreads/Shopping/ShoppingViewController.swift
+++ SeSACRxThreads/Shopping/ShoppingViewController.swift
@@ -26,70 +26,136 @@ final class ShoppingViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        bind()
+       // bind()
+        bind2()
     }
     
-    func bind() {
-        // ShoppingList Behavior Observable Subscirbe
-        viewModel.inputShoppingList.bind(to: mainView.tableView.rx.items(cellIdentifier: "ShoppingTableViewCell", cellType: ShoppingTableViewCell.self)) { (indexPath, item, cell) in
-            
-            cell.productLabel.text = item.item
-            
-//            // 체크
-            cell.doneButton.rx.tap.bind(with: self) { owner, _ in
-                owner.viewModel.inputShoppingValue[indexPath].done.toggle()
-                owner.viewModel.inputShoppingList.accept(owner.viewModel.inputShoppingValue)
-            }
-            .disposed(by: cell.disposeBag)
-
-            // 좋아요
-            cell.likeButton.rx.tap.bind(with: self) { owner, _ in
-                owner.viewModel.inputShoppingValue[indexPath].like.toggle()
-                owner.viewModel.inputShoppingList.accept(owner.viewModel.inputShoppingValue)
-            }
-            .disposed(by: cell.disposeBag)
-            
-            // 삭제
-            cell.deleteButton.rx.tap.bind(with: self, onNext: { owner, _ in
-                owner.viewModel.inputShoppingValue.remove(at: indexPath)
-                owner.viewModel.inputShoppingList.accept(owner.viewModel.inputShoppingValue)
-            })
-            .disposed(by: cell.disposeBag)
-            
-            cell.configureCell(item: item)
-            
-        }
-        .disposed(by: disposeBag)
-
-        // 실시간 검색
-        mainView.textfield.rx
-            .text
-            .orEmpty
-            .subscribe(with: self) { owner, value in
-                owner.viewModel.inputSearchText.onNext(value)
-            }
+    func bind2() {
+        let product = mainView.textfield.rx.text
+        let addTap = mainView.addButton.rx.tap
+        let itemSelected = mainView.tableView.rx.itemSelected
+        let input = ShoppingViewModel.Input(product: product,
+                                            addTap: addTap,
+                                            itemSelected: itemSelected)
+        
+        let output = viewModel.transform(input: input)
+        
+        // 1. 실시간 검색, 데이터 입력값
+        output.product
+            .drive(mainView.textfield.rx.text)
             .disposed(by: disposeBag)
         
-        // 화면 전환
-        mainView.tableView.rx
-            .itemSelected
-            .subscribe(with: self) { owner, indexPath in
-                let vc = EditViewController()
-                owner.viewModel.itemSelected.onNext((vc, indexPath.row))
-                owner.navigationController?.pushViewController(vc, animated: true)
+        // cell
+        output.productList
+            .drive(mainView.tableView.rx.items(cellIdentifier: "ShoppingTableViewCell", cellType: ShoppingTableViewCell.self)) {indexPath, item, cell in
+                cell.productLabel.text = item.item
+               
+                //            // 체크
+//                            cell.doneButton.rx.tap.bind(with: self) { owner, _ in
+//                                owner.viewModel.inputShoppingValue[indexPath].done.toggle()
+//                                owner.viewModel.inputShoppingList.accept(owner.viewModel.inputShoppingValue)
+//                            }
+//                            .disposed(by: cell.disposeBag)
+//                
+//                            // 좋아요
+//                            cell.likeButton.rx.tap.bind(with: self) { owner, _ in
+//                                owner.viewModel.inputShoppingValue[indexPath].like.toggle()
+//                                owner.viewModel.inputShoppingList.accept(owner.viewModel.inputShoppingValue)
+//                            }
+//                            .disposed(by: cell.disposeBag)
+//                
+//                            // 삭제
+//                            cell.deleteButton.rx.tap.bind(with: self, onNext: { owner, _ in
+//                                owner.viewModel.inputShoppingValue.remove(at: indexPath)
+//                                owner.viewModel.inputShoppingList.accept(owner.viewModel.inputShoppingValue)
+//                            })
+//                            .disposed(by: cell.disposeBag)
                 
+                            cell.configureCell(item: item)
             }
             .disposed(by: disposeBag)
         
-        mainView.textfield.rx.text.orEmpty
-            .bind(to: viewModel.inputTextField)
+        // 추가 버튼
+        output.addTap
+            .subscribe(with: self) { owner, _ in
+                print("tap")
+            }
             .disposed(by: disposeBag)
         
+        // tableview item selcted
+        /*
+         output.itemSelected.bind(with: self) { owner, indexPath in
+                             let vc = EditViewController()
+                             owner.navigationController?.pushViewController(vc, animated: true)
+         }
+         .disposed(by: disposeBag)
+         */
+
         
-        mainView.addButton.rx.tap
-            .bind(to: viewModel.addButtonTap)
-            .disposed(by: disposeBag)
+        
     }
+    
+//    func bind() {
+//        // ShoppingList Behavior Observable Subscirbe
+//        viewModel.inputShoppingList.bind(to: mainView.tableView.rx.items(cellIdentifier: "ShoppingTableViewCell", cellType: ShoppingTableViewCell.self)) { (indexPath, item, cell) in
+//            
+//            cell.productLabel.text = item.item
+//            
+////            // 체크
+//            cell.doneButton.rx.tap.bind(with: self) { owner, _ in
+//                owner.viewModel.inputShoppingValue[indexPath].done.toggle()
+//                owner.viewModel.inputShoppingList.accept(owner.viewModel.inputShoppingValue)
+//            }
+//            .disposed(by: cell.disposeBag)
+//
+//            // 좋아요
+//            cell.likeButton.rx.tap.bind(with: self) { owner, _ in
+//                owner.viewModel.inputShoppingValue[indexPath].like.toggle()
+//                owner.viewModel.inputShoppingList.accept(owner.viewModel.inputShoppingValue)
+//            }
+//            .disposed(by: cell.disposeBag)
+//            
+//            // 삭제
+//            cell.deleteButton.rx.tap.bind(with: self, onNext: { owner, _ in
+//                owner.viewModel.inputShoppingValue.remove(at: indexPath)
+//                owner.viewModel.inputShoppingList.accept(owner.viewModel.inputShoppingValue)
+//            })
+//            .disposed(by: cell.disposeBag)
+//            
+//            cell.configureCell(item: item)
+//            
+//        }
+//        .disposed(by: disposeBag)
+//
+//        // 실시간 검색
+//        mainView.textfield.rx
+//            .text
+//            .orEmpty
+//            .subscribe(with: self) { owner, value in
+//                owner.viewModel.inputSearchText.onNext(value)
+//            }
+//            .disposed(by: disposeBag)
+//        
+//        // 화면 전환
+//        mainView.tableView.rx
+//            .itemSelected
+//            .subscribe(with: self) { owner, indexPath in
+//                let vc = EditViewController()
+//                owner.viewModel.itemSelected.onNext((vc, indexPath.row))
+//                owner.navigationController?.pushViewController(vc, animated: true)
+//                
+//            }
+//            .disposed(by: disposeBag)
+//        
+//        mainView.textfield.rx.text.orEmpty
+//            .bind(to: viewModel.inputTextField)
+//            .disposed(by: disposeBag)
+//        
+//        
+//        mainView.addButton.rx.tap
+//            .bind(to: viewModel.addButtonTap)
+//            .disposed(by: disposeBag)
+//    }
 
     
 
--- SeSACRxThreads/Shopping/ShoppingViewModel.swift
+++ SeSACRxThreads/Shopping/ShoppingViewModel.swift
@@ -10,56 +10,140 @@ import RxSwift
 import RxCocoa
 
 class ShoppingViewModel {
-    var inputShoppingValue: [Shopping] = [Shopping(item: "swift 공부", done: false, like: false),
-                                          Shopping(item: "rxSwift 공부", done: false, like: false),
-                                          Shopping(item: "readme 쓰기", done: false, like: false),
-                                          Shopping(item: "블로그 쓰기", done: false, like: false),
-                                          Shopping(item: "업데이트하기", done: false, like: false),
-                                          Shopping(item: "할게 너무 마나...", done: false, like: false)]
+
     
-    var inputShoppingList = PublishRelay<[Shopping]>()
+    /*
+     In
+     1. 텍스트 필드 입력 <실시간 검색, 데이터 추가>
+     2. 셀 클릭
+     3. 체크 버튼 클릭
+     4. 별 버튼 클릭
+     5. 추가 버튼 클릭
+     6. 삭제 버튼 클릭
+     Out
+     1. 입력된 테스트 반환 <실시간 검색, 데이터 추가>
+     2. 셀 클릭
+     3. 체크 버튼 클릭
+     4. 별 버튼 클릭
+     5. 추가 버튼 클릭
+     6. 삭제 버튼 클릭
+     */
+    struct Input {
+        let product: ControlProperty<String?>
+        let addTap: ControlEvent<Void>
+        let itemSelected: ControlEvent<IndexPath>
+//        let checkTap: ControlEvent<Void>
+//        let starTap: ControlEvent<Void>
+//        let deleteTap: ControlEvent<Void>
+        
+    }
     
-    var inputTextField = PublishSubject<String>()
-    var inputSearchText = PublishSubject<String>()
-    let addButtonTap = PublishSubject<Void>()
-    let itemSelected = PublishSubject<(EditViewController, Int)>()
+    struct Output {
+        let product: Driver<String>
+        let productList: Driver<[Shopping]>
+        let addTap: ControlEvent<Void>
+        //let itemSelected: ControlEvent<IndexPath>
+    }
+    
+
     let disposeBag = DisposeBag()
     
-    init() {
-        // 추가 버튼 클릭
-        addButtonTap
-            .withLatestFrom(inputTextField)
-            .subscribe(with: self) { owner, value in
-                owner.inputShoppingValue.append(Shopping(item: value, done: false, like: false))
-                owner.inputShoppingList.accept(owner.inputShoppingValue)
-            }
-            .disposed(by: disposeBag)
+    func transform(input: Input) -> Output {
+        let product = input.product.orEmpty
+        var inputShoppingValue: [Shopping] = [Shopping(item: "swift 공부", done: false, like: false),
+                                              Shopping(item: "rxSwift 공부", done: false, like: false)]
+        let inputShoppingList = BehaviorRelay(value: inputShoppingValue)
         
-        // 실시간 검색
-        inputSearchText
+        product
             .debounce(.seconds(1), scheduler: MainScheduler.instance)
             .distinctUntilChanged()
             .subscribe(with: self) { owner, value in
                 let result = value.isEmpty ?
-                owner.inputShoppingValue :
-                owner.inputShoppingValue.filter { $0.item.contains(value) }
+                inputShoppingValue :
+                inputShoppingValue.filter { $0.item.contains(value) }
                 
-                owner.inputShoppingList.accept(result)
+                inputShoppingList.accept(result)
+            }
+            .disposed(by: disposeBag)
+
+        input.addTap
+            .withLatestFrom(product)
+            .filter { $0 != "" }
+            .subscribe(with: self) { owner, value in
+                inputShoppingValue.append(Shopping(item: value, done: false, like: false))
+                inputShoppingList.accept(inputShoppingValue)
+                
             }
             .disposed(by: disposeBag)
         
-        itemSelected.subscribe(with: self, onNext: { owner, value in
-            self.transition(vc: value.0, indexPath: value.1)
-        })
-        .disposed(by: disposeBag)
+        let productResult = product.asDriver()
+        let productListResult = inputShoppingList.asDriver(onErrorJustReturn: [])
         
-    }
+        input.itemSelected
+            .subscribe(with: self, onNext: { owner, indexPath in
+                let vc = EditViewController()
     
-    func transition(vc: EditViewController,indexPath: Int) {
-        // 메서드를 호출하는 방식
-        vc.closure = { value in
-            self.inputShoppingValue[indexPath].item = value
-            self.inputShoppingList.accept(self.inputShoppingValue)
-        }
+                vc.closure = { value in
+                    inputShoppingValue[indexPath.row].item = value
+                    inputShoppingList.accept(inputShoppingValue)
+                }
+            })
+            .disposed(by: disposeBag)
+                
+        return Output(product: productResult,
+                      productList: productListResult,
+                      addTap: input.addTap)
     }
+
+
+    
+
 }
+
+/*
+ 
+ var inputShoppingList = PublishRelay<[Shopping]>()
+ 
+ var inputTextField = PublishSubject<String>()
+ var inputSearchText = PublishSubject<String>()
+ let addButtonTap = PublishSubject<Void>()
+ let itemSelected = PublishSubject<(EditViewController, Int)>()
+ 
+ init() {
+     // 추가 버튼 클릭
+     addButtonTap
+         .withLatestFrom(inputTextField)
+         .subscribe(with: self) { owner, value in
+             owner.inputShoppingValue.append(Shopping(item: value, done: false, like: false))
+             owner.inputShoppingList.accept(owner.inputShoppingValue)
+         }
+         .disposed(by: disposeBag)
+     
+     // 실시간 검색
+     inputSearchText
+         .debounce(.seconds(1), scheduler: MainScheduler.instance)
+         .distinctUntilChanged()
+         .subscribe(with: self) { owner, value in
+             let result = value.isEmpty ?
+             owner.inputShoppingValue :
+             owner.inputShoppingValue.filter { $0.item.contains(value) }
+             
+             owner.inputShoppingList.accept(result)
+         }
+         .disposed(by: disposeBag)
+     
+     itemSelected.subscribe(with: self, onNext: { owner, value in
+         self.transition(vc: value.0, indexPath: value.1)
+     })
+     .disposed(by: disposeBag)
+     
+ }
+ func transition(vc: EditViewController,indexPath: Int) {
+     // 메서드를 호출하는 방식
+     vc.closure = { value in
+         self.inputShoppingValue[indexPath].item = value
+         self.inputShoppingList.accept(self.inputShoppingValue)
+     }
+ }
+
+ */
